08293bb3bb0945c3744711821e239b3df34cd914
adriangln11 <cesarqqu@gmail.com>
2025-11-03 17:47:33 -0600

    Saltos de linea.

--- src/public/index.js
+++ src/public/index.js
@@ -7,7 +7,7 @@ const paymentData = Object.fromEntries(urlParams.entries())
 function populatePurchaseData() {
 
   document.getElementById('description').textContent = paymentData.x_description || 'Descripción del producto';
-  paymentData.x_description = paymentData.x_description.replace(/\n/g, '');
+  paymentData.x_description = paymentData.x_description.replace(/\\n/g, ' ');
   document.getElementById('reference').textContent = paymentData.x_reference || '-';
   document.getElementById('shop-name').textContent = paymentData.x_shop_name || '-';
 
